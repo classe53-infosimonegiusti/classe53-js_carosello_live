a56c81054ee859f91bc99104414df84afd7a4239
Simone Giusti <info@simonegiusti.it>
2022-01-27 15:05:59 +0100

    milestone 3

--- js/main.js
+++ js/main.js
@@ -6,14 +6,6 @@ const images = [
     'img/05.jpg',
 ];
 
-const name = [
-    'svezia',
-    'italia',
-    'danimarca',
-    'russi',
-    'cina'
-];
-
 let slides = '';
 let bullets = '';
 
@@ -35,12 +27,82 @@ itemsContainer.innerHTML = slides;
 let circleContainer = document.querySelector('.circle-container');
 circleContainer.innerHTML = bullets;
 
-// inizializzo una variabile che mi tenga traccia della slide attiva
+//inizializzo una variabile che mi tenga traccia della slide attiva
 let currentSlide = 0;
+console.log('Sono nella slide numero:' + currentSlide);
 
+//recupero tutte le slide, ed alla prima aggiungo la classe active
 let items = document.getElementsByClassName('item');
 items[currentSlide].classList.add('active');
 
+//recupero tutti i circle, ed al primo aggiungo la classe active
 let circles = document.getElementsByClassName('circle');
 circles[currentSlide].classList.add('active');
+
+
+//gestisco il click su slide seguente
+let next = document.querySelector('.next');
+next.addEventListener('click', 
+    function() {
+
+        if (currentSlide < items.length - 1) {
+
+            items[currentSlide].classList.remove('active');
+            circles[currentSlide].classList.remove('active');
+
+            console.log('Ero nella slide numero:' + currentSlide);
+
+            currentSlide++; //vado alla slide seguente
+
+            console.log('Mi sposto nella slide numero:' + currentSlide);
+
+            items[currentSlide].classList.add('active');
+            circles[currentSlide].classList.add('active');
+
+        } else {
+
+            items[currentSlide].classList.remove('active');
+            circles[currentSlide].classList.remove('active');
+
+            currentSlide = 0; //vai alla prima slide!
+
+            items[currentSlide].classList.add('active');
+            circles[currentSlide].classList.add('active');
+
+        }
+    }
+);
+
+//gestisco il click su slide precedente
+let prev = document.querySelector('.prev');
+prev.addEventListener('click', 
+    function() {
+
+        if (currentSlide > 0) {
+            items[currentSlide].classList.remove('active');
+            circles[currentSlide].classList.remove('active');
+
+            console.log('Ero nella slide numero:' + currentSlide);
+
+            currentSlide--; //vado alla slide precedente
+
+            console.log('Mi sposto nella slide numero:' + currentSlide);
+
+            items[currentSlide].classList.add('active');
+            circles[currentSlide].classList.add('active');
+
+        } else {
+
+            items[currentSlide].classList.remove('active');
+            circles[currentSlide].classList.remove('active');
+            
+            currentSlide = items.length - 1;
+
+            items[currentSlide].classList.add('active');
+            circles[currentSlide].classList.add('active');
+
+        }
+    }
+);
+
 
